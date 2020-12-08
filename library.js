efeb28b31277cb232ffe9ca275df1bbfcd9eac25
zeetbor <rob-terrins@hotmail.co.uk>
2020-12-08 21:06:26 +0000

    Added working functionality to add new  books to the library and display on screen

--- library.js
+++ library.js
@@ -1,5 +1,4 @@
-let myLibrary = [
-  {
+let myLibrary = [{
     title: "The Hobbit",
     author: "JRR Tolkien",
     pages: 295,
@@ -14,36 +13,43 @@ let myLibrary = [
 ];
 
 const bookcase = document.querySelector(".bookcase");
-var numberOfBooks = myLibrary.length;
+let numberOfBooks = myLibrary.length;
 
 //Book object constructor
-function Book(title, author, pages, read){
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = () => {
-    return `${this.title} by ${this.author}, ${this.pages}, read: ${this.read}`;
-  }
+  // this.info = function(){
+  //   return `${this.title} by ${this.author}, ${this.pages}, read: ${this.read}`;
+  // }
 };
 
-
-addBookToLibrary = (title, author, pages, read) => {
-  const addBook = new Book(title, author, pages, read); //create new book
+function addBookToLibrary(title, author, pages, read) {
+  addBook = new Book(title, author, pages, read); //add to library
+  myLibrary.push(addBook); //create new book
   addBook.prototype = Object.create(Book.prototype); //set prototype
-  myLibrary.push(addBook); //add to library
-}
+  createCard(myLibrary.length - 1);
+};
 
 //Populate page with book cards
-render = () => {
-  for (i = 0; i < numberOfBooks; i++) {
-    createCard(i);
-  };
-};
-
 createCard = (index = numberOfBooks - 1) => {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   const card = document.createElement('div');
   card.setAttribute("class", "card")
+  card.setAttribute('data-index', index);
+  card.style.backgroundColor = randomColor;
+
+  const remove = document.createElement("button");
+  remove.textContent = "X";
+  remove.setAttribute("class", "remove");
+  remove.setAttribute("id", "close");
+  remove.addEventListener("click", (e) => {
+    myLibrary.splice(remove.parentElement.getAttribute("data-index"), 1);
+    e.srcElement.parentElement.remove();
+  });
+  card.appendChild(remove);
 
   const title = document.createElement("h2");
   title.textContent = myLibrary[index].title;
@@ -65,12 +71,68 @@ createCard = (index = numberOfBooks - 1) => {
   read.setAttribute("class", "info");
   card.appendChild(read);
 
+  const readBtn = document.createElement("button");
+  readBtn.textContent = "Change read status"
+  readBtn.setAttribute("class", "changBtn");
+  card.appendChild(readBtn);
+  readBtn.addEventListener("click", () => {
+    if (myLibrary[index].read === "Yes") {
+      myLibrary[index].read = "No";
+      readBtn.parentElement.children[4].textContent = "Already Read: No"
+    } else {
+      myLibrary[index].read = "Yes";
+      readBtn.parentElement.children[4].textContent = "Already Read: Yes"
+    }
+  });
   bookcase.appendChild(card);
 };
 
+
+
+
 //Add event listeners to buttons
+const showForm = document.querySelector(".form-container");
+
 const new_button = document.querySelector(".new");
-new_button.addEventListener('click', render());
+new_button.addEventListener('click', (e) => {
+  showForm.style.display = "inline-block";
+});
+
+const closeForm = document.querySelector(".closeBtn");
+closeForm.addEventListener('click', (e) => {
+  showForm.style.display = "none";
+});
 
 const submit_button = document.querySelector("#submit");
-submit_button.addEventListener('click', addBookToLibrary());
+submit_button.addEventListener('click', (e) => {
+  const form = document.querySelector(".form");
+    addBookToLibrary(form.title.value, form.author.value, form.pages.value, form.read.value);
+    resetForm(form);
+    showForm.style.display = "none";
+});
+
+function resetForm(form){
+    form.title.value = "";
+    form.author.value = "";
+    form.pages.value = "";
+}
+
+render = () => {
+    for (i = 0; i < numberOfBooks; i++) {
+      createCard(i);
+    };
+  };
+render();
+
+
+/*
+const card = document.querySelector(".card");
+const remove_button = document.querySelectorAll(".remove");
+for (let i = 0; i < remove_button.length; i++) {
+  remove_button[i].addEventListener("click", () => {
+    let bookIndex = card.getAttribute('data-index');
+    let bookNode = document.querySelector(`.card[data-index='${bookIndex}']`);
+    document.querySelector('.card').parentNode.removeChild(bookNode);
+    myLibrary.splice(myLibrary.indexOf(card), 1);
+  })};
+*/
